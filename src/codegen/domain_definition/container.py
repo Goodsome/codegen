a951a3c62fcc7a559cb5ff058260a83d6fc44f99
Goodsome <xlxmesh@gmail.com>
2026-03-31 11:14:21 +0800

    refactor: remove unused GetValue/SetValue/RemoveValue use cases and their dependencies
    Remove the generic path-based use cases (GetValue, SetValue, RemoveValue) that
    are no longer used, along with their upstream dependencies:
    - BlueprintPathOperations
    - BlueprintPathResolver
    - PathSegment
    
    Also cleaned up container.py by removing the orphaned path_resolver and
    path_operations providers.

--- src/codegen/domain_definition/container.py
+++ src/codegen/domain_definition/container.py
@@ -80,20 +80,11 @@ from codegen.domain_definition.application.use_cases.get_http_endpoint import Ge
 from codegen.domain_definition.application.use_cases.remove_http_endpoint import RemoveHttpEndpoint
 
 # Other use cases
-from codegen.domain_definition.application.use_cases.get_value import GetValue
 from codegen.domain_definition.application.use_cases.init_project import InitProject
 from codegen.domain_definition.application.use_cases.load_blueprint import LoadBlueprint
 from codegen.domain_definition.application.use_cases.remove_context import RemoveContext
-from codegen.domain_definition.application.use_cases.remove_value import RemoveValue
-from codegen.domain_definition.application.use_cases.set_value import SetValue
 from codegen.domain_definition.application.use_cases.upsert_context import UpsertContext
 
-from codegen.domain_definition.domain.services.blueprint_path_operations import (
-    BlueprintPathOperations,
-)
-from codegen.domain_definition.domain.services.blueprint_path_resolver import (
-    BlueprintPathResolver,
-)
 from codegen.domain_definition.infrastructure.adapters.yaml_blueprint_storage import (
     YamlBlueprintStorage,
 )
@@ -107,24 +98,7 @@ class Container(DeclarativeContainer):
         config_path=config.config_path,
     )
 
-    path_resolver = Singleton(BlueprintPathResolver)
-
-    path_operations = Singleton(
-        BlueprintPathOperations,
-        resolver=path_resolver,
-    )
-
     load_blueprint = Factory(LoadBlueprint, blueprint_loader=yaml_blueprint_storage)
-
-    get_value = Factory(
-        GetValue,
-        storage=yaml_blueprint_storage,
-        operations=path_operations,
-    )
-
-    set_value = Factory(SetValue, storage=yaml_blueprint_storage, operations=path_operations)
-
-    remove_value = Factory(RemoveValue, storage=yaml_blueprint_storage, operations=path_operations)
 
     remove_context = Factory(RemoveContext, storage=yaml_blueprint_storage)
 
